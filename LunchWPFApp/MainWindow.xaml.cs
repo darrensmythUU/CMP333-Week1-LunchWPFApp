46389b1241281c092284da36196246bd02d94554
Kiphea <djsmyth05@gmail.com>
2026-05-05 01:49:46 +0100

    Added select button.

--- LunchWPFApp/MainWindow.xaml.cs
+++ LunchWPFApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace LunchWPFApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
