26b4be5dfc41a573a9850f2d5f0a31fd496f7eea
Kiphea <djsmyth05@gmail.com>
2026-05-05 01:54:22 +0100

    Added Select button + functionality to showcase message box showing what lunch option is picked when Select button is clicked.

--- LunchWPFApp/MainWindow.xaml.cs
+++ LunchWPFApp/MainWindow.xaml.cs
@@ -27,7 +27,26 @@ namespace LunchWPFApp
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            if (wrapRadioButton.IsChecked == true)
+            {
+                MessageBox.Show("I am going to have a wrap for lunch today");
+            }
+            else if (burritoRadioButton.IsChecked == true)
+            {
+                MessageBox.Show("I am going to have a burrito for lunch today");
+            }
+            else if (tacoRadioButton.IsChecked == true)
+            {
+                MessageBox.Show("I am going to have a taco for lunch today");
+            }
+            else if (paniniRadioButton.IsChecked == true)
+            {
+                MessageBox.Show("I am going to have a panini for lunch today");
+            }
+            else if (burgerRadioButton.IsChecked == true)
+            {
+                MessageBox.Show("I am going to have a burger for lunch today");
+            }
         }
     }
 }
